1d769799d4aafb164ac8cfc5f073bb5af9094327
Marie-Isabelle Schonborn <marieisabelleschoenborn@gmail.com>
2020-06-16 16:32:26 +0100

    tasks app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :task_new
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  patch 'tasks/:id', to: 'tasks#update'
+  post 'tasks', to: 'tasks#create'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
